56dcd5ad422a36d7849f380115f9c2be2dfa3746
oldgreyduck <caquelin@hey.com>
2025-05-09 19:11:44 -0400

    Final changes to bookbot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import count_words
 from stats import count_chars
+from stats import sort_char
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -10,7 +11,15 @@ def main():
     text = get_book_text(filepath)
     num_words = count_words(text)
     num_chars = count_chars(text)
-    print(f"{num_words} words found in the document")
-    print(num_chars)
+    char_list = sort_char(num_chars)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in char_list:
+        if item["char"].isalpha():
+            print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -9,3 +9,18 @@ def count_chars(text):
             unique_chars[char] = unique_chars[char] + 1
         else: unique_chars[char] = 1
     return unique_chars
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_char(count_chars):
+    sort_list = []
+    for char in count_chars:
+        count = count_chars[char]
+        sort_list.append({"char": char, "num": count})
+    sort_list.sort(reverse=True, key=sort_on)
+    return sort_list
+
+
+    
+    
